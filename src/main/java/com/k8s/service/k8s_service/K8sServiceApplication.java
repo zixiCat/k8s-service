f438a43e5f3abe115a9fcda4412135ee29b06d50
zixiCat <zixicat@outlook.com>
2024-12-30 10:47:32 +0800

    feat: link start

---
+++ src/main/java/com/k8s/service/k8s_service/K8sServiceApplication.java
@@ -0,0 +1,13 @@
+package com.k8s.service.k8s_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(K8sServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/k8s/service/k8s_service/K8sServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.k8s.service.k8s_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
